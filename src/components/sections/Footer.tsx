ca593e4213b1088fe9a0fd835ecff953050e4265
holadimepartner-glitch <hola.dimepartner@gmail.com>
2026-05-09 12:02:01 -0400

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,6 +1,5 @@
 import partnersLogoFull from "../../assets/partners-logo-footer.png";
 import { Linkedin, Instagram } from "lucide-react";
-
 const navLinks = [
   { label: "Servicios", id: "servicios" },
   { label: "Metodología", id: "metodologia" },
